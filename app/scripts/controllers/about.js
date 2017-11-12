7739e3b37af9cf4eae0a39264be99dffc945eb2a
Eric Klinkhammer <eklinkhammer@gmail.com>
2017-11-12 15:30:34 -0800

    Pushing scaffold to source.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qualityCareApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the qualityCareApp
+ */
+angular.module('qualityCareApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qualityCareApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the qualityCareApp
+ */
+angular.module('qualityCareApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
